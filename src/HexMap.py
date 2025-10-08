09c44de9b9dbcf7c80ff808317d257615147466e
Hyilix <sebi.ursescu@gmail.com>
2025-10-08 15:47:30 +0300

    More hexmap/state functions, assets folder

--- src/HexMap.py
+++ src/HexMap.py
@@ -1,5 +1,4 @@
 from Hex import Hex
-from Hex import Unit
 
 # Default hex
 empty_hex = Hex(0, 0, -1)
@@ -10,6 +9,72 @@ class HexMap:
         self.hexmap = [[empty_hex] * x_tile_count] * y_tile_count
 
     def get_hex_from_pos(self, x_pos : int, y_pos : int):
+        # Out of bounds check
+        if x_pos < 0 or y_pos < 0 or x_pos >= self.dimensions[0] or y_pos >= self.dimensions[1]:
+            return None
         return self.hexmap[y_pos][x_pos]
 
+    def get_hexmap(self):
+        return self.hexmap
+
+    # Returns the position of a hex in the array
+    def get_pos_from_hex(self, tile : Hex):
+        for y in range(self.dimensions[1]):
+            for x in range(self.dimensions[0]):
+                if self.hexmap[y][x] == tile:
+                    return (x, y)
+        return (None, None)
+
+    # Returns a neighbor of input index
+    def get_hex_neighbor(self, tile : Hex, index : int):
+        (tile_pos_x, tile_pos_y) = self.get_pos_from_hex(tile)
+        index = index % 6
+
+        # Check if tile exists
+        if tile_pos_x == None or tile_pos_y == None:
+            return None
+
+        if index == 0:
+            tile_pos_y -= 1
+        elif index == 1:
+            tile_pos_x += 1
+        elif index == 2:
+            tile_pos_x += 1
+            tile_pos_y += 1
+        elif index == 3:
+            tile_pos_y += 1
+        elif index == 4:
+            tile_pos_y += 1
+            tile_pos_x -= 1
+        elif index == 5:
+            tile_pos_x -= 1
+
+        return self.get_hex_from_pos(tile_pos_x, tile_pos_y)
+
+    # Returns an array containing all 6 neighbors of a hex
+    def get_hex_all_neighbors(self, tile : Hex):
+        neighbors = []
+        for index in range(6):
+            neighbors.append(self.get_hex_neighbor(tile, index))
+        return neighbors
+
     # TODO: add unit movement (or get the hexes that a unit can move to)
+
+# Index of each neighbour
+#            _____
+#           /     \
+#          /       \
+#    ,----(    0    )----.
+#   /      \       /      \
+#  /        \_____/        \
+#  \    5   /     \   1    /
+#   \      /       \      /
+#    )----(   self  )----(
+#   /      \       /      \
+#  /    4   \_____/   2    \
+#  \        /     \        /
+#   \      /       \      /
+#    `----(    3    )----'
+#          \       /
+#           \_____/
+
--- src/State.py
+++ src/State.py
@@ -1,5 +1,10 @@
+from collections import deque
+
 from Hex import Hex
 from HexMap import HexMap
+
+def __hex_march_step__(tile : Hex, queue : deque[Hex]):
+    pass
 
 class State:
     def __init__(self, owner : int, central_hex : Hex):
@@ -26,8 +31,26 @@ class State:
         self.central_hex = central_hex
 
     # Get the hexes of a state starting from the central hex
-    def hex_march(self):
-        pass
+    def hex_march(self, hexmap : HexMap):
+        hexqueue = deque()
+        hexqueue.append(self.central_hex)
+
+        visited = [self.central_hex]
+
+        # Search all the hexes for a state
+        while hexqueue[0]:
+            neighbors = hexmap.get_hex_all_neighbors(hexqueue[0])
+            for tile in neighbors:
+                if tile.get_owner() == self.owner and tile not in visited:
+                    hexqueue.append(tile)
+                    visited.append(tile)
+
+            hexqueue.popleft()
+
+        # Copy the visited hexes onto the state_hexes
+        self.state_hexes = visited[:]
+
 
     # TODO: class methods for splitting, merging states
     # TODO: class methods for getting the hexes of states, adding/removing hexes from states
+
