41ae504ed5fb07ff90c08eb77706527e825bf7e0
Hyilix <sebi.ursescu@gmail.com>
2025-11-17 13:17:28 +0200

    More buttons

--- src/button.py
+++ src/button.py
@@ -1,11 +1,40 @@
+from os import posix_fadvise
 import pygame
 
+import colors
+
+# Super class for all types of buttons
 class Button:
-    def __init__(self, a , b):
-        self.__a = a
-        self.__b = b
-        self.buttonBG = pygame.Rect(self.__a, self.__b, 50, 25)
-        self.button = pygame.Rect(self.__a + 2, self.__b + 2, 46, 21)
+    def __init__(self, pos : tuple[int, int], size : tuple[int, int], func : function):
+        self.__pos = pos
+        self.__size = size
+        self.__function = func
+
+    def change_pos(self, new_pos : tuple[int, int]):
+        self.__pos = new_pos
+
+    def change_size(self, new_size : tuple[int, int]):
+        self.__size = new_size
+
+    def get_pos(self):
+        return self.__pos
+
+    def get_size(self):
+        return self.__size
+
+    def change_function(self, new_func : function):
+        self.__function = new_func
+
+    def call_function(self, *args, **kwargs):
+        if self.__function is not None:
+            self.__function(*args, **kwargs)
+
+# Button made from 2 rectangles
+class SimpleButton(Button):
+    def __init__(self, pos : tuple[int, int], size : tuple[int, int], func : function = None):
+        super().__init__(pos, size, func)
+        self.buttonBG = pygame.Rect(self.__pos, self.__size)
+        self.button = pygame.Rect(self.__pos[0] + 2, self.__pos[1] + 2, self.__size[0] - 4, self.__size[1] - 4)
 
     def draw(self, screen, font, color1, color2, color3, text):
         pygame.draw.rect(screen, color1, self.buttonBG)
@@ -14,4 +43,20 @@ class Button:
         text_surface = font.render(text, True, color3)
         text_rect = text_surface.get_rect(center=self.buttonBG.center)
         screen.blit(text_surface, text_rect)
+
+# Button having a texture
+class TextureButton(Button):
+    def __init__(self, pos : tuple[int, int], size : tuple[int, int], func : function):
+        super().__init__(pos, size, func)
+
+        # Information about button highlight
+        self.__highlight_color = colors.white
+        self.__highlight_alpha = 200
+        self.__is_highlighted = False
+
+        # Information about button texture (including highlight)
+        self.__texture : pygame.Surface = pygame.Surface(self.__size, pygame.SRCALPHA)
+
+    def load_texture(self, texture : pygame.Surface):
+        pass
 
